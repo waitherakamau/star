74c8cdf28481df80de2b00a38d15d446e605e348
waitherakamau <effencekamau@gmail.com>
2022-03-30 15:46:29 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val names:Array<String> = arrayOf("jim","john","jamie")
+    //names [0]="jeremy"
+    //println("Number of elements:${names.size}")
+    //print("First element:${names[0]}")
+    //println("last element: ${names[2]}")
+    //println(names[0])
+    //println(names[1])
+   // println(names[2])
+
+    //for (names:String in names){
+        println(names)
+        // for (i : Int in 1..10)println(i)
+        // for(i : Int in 1 until 10) println(i)
+         for(i:Int in 1..10 step 2) println(i)
+
+    }
+
+
+
+//}
